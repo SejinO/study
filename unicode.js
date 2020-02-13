174e22119160c62564a6ec12adac330dbc0da079
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-02-14 03:23:13 +0900

    Update unicode.js

--- unicode.js
+++ unicode.js
@@ -21,6 +21,10 @@ background(0);
     textSize(20);
     fill(255);
   text("shake it",width/2,height-50);
+	    textSize(10);
+
+	  text("The device has not been running since the iPhone X",width/2,height-40);
+
 }
 function deviceShaken(){
   let r = floor(random(0xAC00,0xD7A3));
