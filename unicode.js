e7a32c4f4ceb2599554a0ffbf4febe7c571f4dfe
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-03-03 04:38:01 +0900

    Update unicode.js

--- unicode.js
+++ unicode.js
@@ -2,6 +2,7 @@
 SourceCode_
 github.com/designerSejinOH/Live
 */
+let permissionGranted = false;
 
 let myFont;
 let code;
@@ -13,13 +14,49 @@ function setup() {
 createCanvas(windowWidth,windowHeight);
 code = 0xAC00;
   textAlign(CENTER);
-
-
+	
+	 //DeviceOrientationEvent, DeviceMotionEvent
+  if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
+    //ios 13 device
+    DeviceOrientationEvent.requestPermission()
+    .catch(() => {
+    let button = createButton("click to allow to sensors");
+    button.style("font-size", "24px");
+    button.center();
+    button.mousePressed(requsetAccess);
+      throw error;
+    })
+    .then(() => {
+      // on any subsequent visits
+      permissionGranted = true;
+    })
+  } else {
+    //non ios 13 device
+    textSize(48);
+    // text("non ios 13 device", 100, 100);
+    permissionGranted = true;
+  }
+}
+function requsetAccess() {
+  DeviceOrientationEvent.requestPermission()
+    .then(response => {
+        if (response == 'granted') {
+          permissionGranted = true;
+        }else{
+          permissionGranted = false;
+        }
+      })
+    .catch(console.error);
+  this.remove();
 }
 function windowResized() {
 			resizeCanvas(windowWidth, windowHeight);
 		}
 function draw() {
+	if (!permissionGranted) {
+    return;
+  }
+	
 background(0);
   textFont(myFont);
   textSize(200);
